1a430ee248ed15351c92fb7573e7665f8e5c5065
hyurai <yuumanagai0504@icloud.com>
2021-08-29 05:54:37 +0000

    users_add_column_name

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,6 @@ class TweetsController < ApplicationController
   end
   def create
     tweet = Tweet.create(user_id: params[:user_id],text: params[:text])
-    redirect_to '/tweet'
+    redirect_to '/tweets'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_044104) do
+ActiveRecord::Schema.define(version: 2021_08_29_053147) do
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_044104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
