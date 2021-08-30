76b2c7d79cc7eeb6eff21114a5bb222f7020ae1e
hyurai <yuumanagai0504@icloud.com>
2021-08-30 06:49:37 +0000

    github_login_base

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
+  def github
+    @user = User.find_for_github_oauth(request.env["omniauth.auth"])
+    
+    if @user.persisted?
+      sign_in_and_redirect @user,event: :authentication
+      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 
   # You should also create an action method in this controller like this:
   # def twitter
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :email,presence: true,uniqueness: true
-  validates :uid,presence: true,uniqueness: { scope: :provider }
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
